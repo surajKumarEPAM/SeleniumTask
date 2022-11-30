71a4d4a612ec322a01c0a0d04b4ed17e69f7c05a
Suraj Kumar <suraj_kumar@epam.com>
2022-11-30 22:53:42 +0530

    Task One for launching multiple browser using differnt classes

---
+++ TaskOneBrowserLaunch/SeleniumCopy/FireFox.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace SeleniumCopy
+{
+    internal class FireFox
+    {
+        public void FireFoxBrowser()
+        {
+           
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            Console.WriteLine(driver.Title);
+            IWebElement signInBtn = driver.FindElement(By.XPath("//a[contains(text(),'Sign in')]"));
+            signInBtn.Click();
+            Console.WriteLine(driver.Title);
+            IWebElement EmailOrPhone = driver.FindElement(By.XPath("//input[@id='identifierId']"));
+            if (EmailOrPhone.Displayed)
+            {
+                Console.WriteLine("Email or Phone input box is displayed on sign page");
+            }
+            else
+            {
+                Console.WriteLine("Email or Phone input box is not displayed on the sign page");
+            }
+            driver.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TaskOneBrowserLaunch/SeleniumCopy/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using SeleniumCopy;
+
+namespace SeleniumCopy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chrome chrome= new Chrome();
+            //chrome.ChromeBrowser();
+            //Edge edge = new Edge();
+            //edge.EdgeBrowser();
+            FireFox fireFox = new FireFox();
+            fireFox.FireFoxBrowser();
+            Console.ReadLine();
+            
+        }
+    }
+}

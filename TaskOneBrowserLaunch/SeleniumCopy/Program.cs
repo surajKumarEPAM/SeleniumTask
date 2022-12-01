3378725851f63fded22dd9734a0d2c55057d86dd
Suraj Kumar <suraj_kumar@epam.com>
2022-12-01 07:37:03 +0530

    Task One for launching multiple browser using different classes

--- TaskOneBrowserLaunch/SeleniumCopy/Program.cs
+++ TaskOneBrowserLaunch/SeleniumCopy/Program.cs
@@ -9,10 +9,13 @@ namespace SeleniumCopy
     {
         static void Main(string[] args)
         {
+            //Using Chrome Driver
             //Chrome chrome= new Chrome();
             //chrome.ChromeBrowser();
+            //Using Edge Driver
             //Edge edge = new Edge();
             //edge.EdgeBrowser();
+            //Using Firefox driver
             FireFox fireFox = new FireFox();
             fireFox.FireFoxBrowser();
             Console.ReadLine();

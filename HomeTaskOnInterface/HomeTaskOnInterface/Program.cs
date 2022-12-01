2f0f3682e2cc72d9f45a799bc71606176c52bc26
Suraj Kumar <suraj_kumar@epam.com>
2022-12-01 09:13:39 +0530

    Home task on Interface containg same method(s)

---
+++ HomeTaskOnInterface/HomeTaskOnInterface/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskOnInterface
+{
+    interface IRestaurant
+    {
+        void eating();
+        void ordering();
+       
+    }
+    interface IHotel
+    {
+        void eating();
+        void ordering();
+    }
+    class MyClass: IRestaurant,IHotel
+    {
+         void IRestaurant.eating() {
+            Console.WriteLine("eating in Restaurant");
+        }
+         void IHotel.eating()
+        {
+            Console.WriteLine("eating in Hotel");
+        }
+         void IRestaurant.ordering() {
+            Console.WriteLine("Ordering in Restaurant");
+        }
+        void IHotel.ordering()
+        {
+            Console.WriteLine("Ordering in hotel");
+        }
+    }
+    internal class Program
+    {
+        //Eat and order
+        
+        static void Main(string[] args)
+        {
+            IHotel myhotel= new MyClass();
+            myhotel.eating();
+            myhotel.ordering();
+            IRestaurant myrestaurant = new MyClass();
+            myrestaurant.eating();
+            myrestaurant.ordering();
+            Console.ReadLine();
+            
+        }
+    }
+}

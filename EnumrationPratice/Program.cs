c01493772246d07a3e0c5211f0cab09fb81762bd
Suraj Kumar <suraj_kumar@epam.com>
2022-12-14 07:40:06 +0530

    Practice on enumration basic program

---
+++ EnumrationPratice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumrationPratice
+{
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thrusday, Friday
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Days d = (Days)1;
+            Console.WriteLine(d);
+            Console.ReadLine();
+        }
+    }
+}

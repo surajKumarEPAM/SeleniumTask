932b7c3d87630822035c2a77f821db58cc227eb0
Suraj Kumar <suraj_kumar@epam.com>
2022-12-14 08:48:46 +0530

    practice on singleton class

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ankesh");
+            int a = 10;
+            Console.WriteLine(a.GetType().Name);
+            Console.Read();
+            
+
+        }
+    }
+}

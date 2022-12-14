f06f08a41aea0a43468f1ec8a58f46bd092ce11a
Suraj Kumar <suraj_kumar@epam.com>
2022-12-14 08:42:52 +0530

    practice on the static keyword

---
+++ PracticingStatic/PracticingStatic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticingStatic
+{
+    class StaticClassDemo
+    {
+        
+        public static int x = 20;
+        public int y = 30;
+        public static void fun()
+        {
+            Console.WriteLine("This is fun1 in static class"+new StaticClassDemo().y);
+        }
+    }
+
+    internal class Program
+    {
+        static int x = 10;
+
+        public  void dispay()
+        {
+            Console.WriteLine(x+4);
+        }
+        static void Main(string[] args)
+        {
+            Program program= new Program();
+           StaticClassDemo sc=new StaticClassDemo();
+            Console.WriteLine(Program.x);
+            Console.WriteLine(sc.y);
+           // sc.fun();
+          StaticClassDemo.fun();
+            Console.ReadLine();
+        }
+    }
+}

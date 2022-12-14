b16e3900409f0aea659dc1a12dbcda9279a29155
Suraj Kumar <suraj_kumar@epam.com>
2022-12-14 07:37:31 +0530

    Practice on Filehandling

---
+++ EnnumsDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnnumsDemo
+{
+    public enum Days
+    {
+        Mon,
+        Tue,
+        Wed,
+        Thr,
+        Fri,
+        Sat,
+        Sun
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.WriteLine("This is Ram");
+
+
+            // Days d = (Days)1;
+            // Console.WriteLine(d);
+
+            //Days d = Days.Thr;
+            //Console.WriteLine((int)d);
+
+            //foreach(string i in Enum.GetNames(typeof(Days)))
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //foreach (int i in Enum.GetValues(typeof(Days)))
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}

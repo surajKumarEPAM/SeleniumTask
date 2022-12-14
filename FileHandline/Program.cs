15f1c2a8a288cfabd69430c25d801267c84d6081
Suraj Kumar <suraj_kumar@epam.com>
2022-12-14 07:53:50 +0530

    Practice on the Filehandling FileStream class with Streamwriter

---
+++ FileHandline/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace FileHandline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Suraj_Kumar\Desktop\myFile3";
+           using(FileStream fs = new FileStream(path,FileMode.Open,FileAccess.Read))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                   // String line = "";
+                    //while((line=sr.ReadLine())!=null)
+                        //Console.WriteLine(line);
+                    //Console.WriteLine(line);
+                    Console.WriteLine(sr.ReadToEnd());
+                    Console.WriteLine(sr.Peek());
+                }
+            }
+             Console.ReadLine();
+        }
+    }
+}
